f0aa1680560efc71538e5780f970895adcf0a665
SteveBlance <stephen.blance@gmail.com>
2014-11-16 23:20:13 +0000

    New User entity

---
+++ src/com/codaconsultancy/model/User.java
@@ -0,0 +1,30 @@
+package com.codaconsultancy.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    private Long id;
+    private String password;
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
