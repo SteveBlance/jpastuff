3905f2d37c5de2c36ed0f0d9723a275476516cab
SteveBlance <stephen.blance@gmail.com>
2014-11-18 00:36:03 +0000

    Added temporary main method for Session and Factory etc

--- src/com/codaconsultancy/model/User.java
+++ src/com/codaconsultancy/model/User.java
@@ -1,5 +1,11 @@
 package com.codaconsultancy.model;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -26,5 +32,23 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration();
+        configuration.addAnnotatedClass(User.class);
+        configuration.configure();
+//        new SchemaExport(configuration).create(true, true);
+        ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(
+                configuration.getProperties()).buildServiceRegistry();
+        SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+
+        User user = new User();
+        user.setPassword("abc124");
+
+        session.saveOrUpdate(user);
+        session.getTransaction().commit();
     }
 }
