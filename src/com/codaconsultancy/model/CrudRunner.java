a9f32f5565a520648c99f1a248b2e5a09d4ff87e
SteveBlance <stephen.blance@gmail.com>
2014-11-21 19:48:06 +0000

    Added HibernateUtil class.

--- src/com/codaconsultancy/model/CrudRunner.java
+++ src/com/codaconsultancy/model/CrudRunner.java
@@ -2,10 +2,6 @@ package com.codaconsultancy.model;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
 
 import java.util.List;
 
@@ -17,12 +13,10 @@ public class CrudRunner {
 //        CrudRunner.retrieveById(1L);
 //        CrudRunner.updateAll();
 //        CrudRunner.deleteAll();
-
     }
 
     private static void deleteAll() {
-        Session session = getSession();
-        session.beginTransaction();
+        Session session = HibernateUtil.beginTransaction();
 
         Query queryResult = session.createQuery("from User");
 
@@ -31,12 +25,11 @@ public class CrudRunner {
         for (User user : allUsers) {
             session.delete(user);
         }
-        session.getTransaction().commit();
+        HibernateUtil.commitTransaction();
     }
 
     private static void updateAll() {
-        Session session = getSession();
-        session.beginTransaction();
+        Session session = HibernateUtil.beginTransaction();
 
         Query queryResult = session.createQuery("from User");
 
@@ -46,12 +39,11 @@ public class CrudRunner {
             user.setPassword("password");
             session.update(user);
         }
-        session.getTransaction().commit();
+        HibernateUtil.commitTransaction();
     }
 
     private static void retrieveById(Long id) {
-        Session session = getSession();
-        session.beginTransaction();
+        Session session = HibernateUtil.beginTransaction();
 
         Query queryResult = session.createQuery("from User where id = :id");
         queryResult.setLong("id", id);
@@ -59,12 +51,11 @@ public class CrudRunner {
         User user = (User) queryResult.uniqueResult();
 
         System.out.println(user.getPassword());
-        session.getTransaction().commit();
+        HibernateUtil.commitTransaction();
     }
 
     private static void retrieve() {
-        Session session = getSession();
-        session.beginTransaction();
+        Session session = HibernateUtil.beginTransaction();
 
         Query queryResult = session.createQuery("from User");
 
@@ -74,27 +65,17 @@ public class CrudRunner {
             System.out.println(user.getPassword());
         }
 
-        session.getTransaction().commit();
+        HibernateUtil.commitTransaction();
     }
 
     private static void create() {
-        Session session = getSession();
-        session.beginTransaction();
+        Session session = HibernateUtil.beginTransaction();
 
         User user = new User();
         user.setPassword("abc160");
 
         session.save(user);
-        session.getTransaction().commit();
+        HibernateUtil.commitTransaction();
     }
 
-    private static Session getSession() {
-        Configuration configuration = new Configuration();
-        configuration.addAnnotatedClass(User.class);
-        configuration.configure();
-        ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(
-                configuration.getProperties()).buildServiceRegistry();
-        SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-        return sessionFactory.getCurrentSession();
-    }
 }
